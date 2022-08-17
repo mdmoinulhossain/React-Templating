08735a4f1bc58788b737ba098a6db3d24fd47fbe
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2022-08-17 18:27:41 +0600

    edited

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,73 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./Home.css";
+import img from "../image/Headphone.jpg";
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Hellow</h1>
+  render() {
+    return (
+      <>
+        <main className="home_main">
+          <section className="card_body">
+            <img src={img} alt="headphone" className="product_img" />
+            <p className="product_name">
+              Micro Fiber Bike/Car Cleaning Towel Double Layer 30*30cm
+            </p>
+            <div className="product_footer">
+              <p className="product_cp">Tk. 500</p>
+              <p className="product_op">
+                Tk. 800 <span className="product_dc">-20%</span>
+              </p>
+              <a href=" " className="wishlist">
+                &#10084;&#65039; Wishlist
+              </a>
             </div>
-        )
-    }
+          </section>
+          <section className="card_body">
+            <img src={img} alt="headphone" className="product_img" />
+            <p className="product_name">Micro Fiber Bike/Car Cleaning.</p>
+            <div className="product_footer">
+              <p className="product_cp">Tk. 500</p>
+              <p className="product_op">
+                Tk. 800 <span className="product_dc">-20%</span>
+              </p>
+              <a href=" " className="wishlist">
+                &#10084;&#65039; Wishlist
+              </a>
+            </div>
+          </section>
+          <section className="card_body">
+            <img src={img} alt="headphone" className="product_img" />
+            <p className="product_name">
+              Micro Fiber Bike/Car Cleaning Towel Double Layer 30*30cm. Towel
+              Double Layer 30*30cm. Towel Double Layer 30*30cm.
+            </p>
+            <div className="product_footer">
+              <p className="product_cp">Tk. 500</p>
+              <p className="product_op">
+                Tk. 800 <span className="product_dc">-20%</span>
+              </p>
+              <a href=" " className="wishlist">
+                &#10084;&#65039; Wishlist
+              </a>
+            </div>
+          </section>
+          <section className="card_body">
+            <img src={img} alt="headphone" className="product_img" />
+            <p className="product_name">
+              Micro Fiber Bike/Car Cleaning Towel Double Layer 30*30cm
+            </p>
+            <div className="product_footer">
+              <p className="product_cp">Tk. 500</p>
+              <p className="product_op">
+                Tk. 800 <span className="product_dc">-20%</span>
+              </p>
+              <a href=" " className="wishlist">
+                &#10084;&#65039; Wishlist
+              </a>
+            </div>
+          </section>
+        </main>
+      </>
+    );
+  }
 }
